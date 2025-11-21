af2224b9544ef2b080d389f89db4a88ed1782509
Melody Gaillard <melody.gaillard@outlook.com>
2025-11-21 13:49:54 +0100

    Aded crowd sound effect

--- Assets/Scripts/Sound/CrowdPlayer.cs
+++ Assets/Scripts/Sound/CrowdPlayer.cs
@@ -1,4 +1,6 @@
-﻿using Core;
+﻿using System;
+using System.Linq;
+using Core;
 using UnityEngine;
 using Event = Core.Event;
 
@@ -6,6 +8,7 @@ namespace Sound
 {
     public class CrowdPlayer : MonoBehaviour
     {
+        [SerializeField] private Parties party;
         private AudioSource _audioSource;
 
         private void Awake()
@@ -16,11 +19,30 @@ namespace Sound
         private void Start()
         {
             Results.Instance.OnResultsGot += PlaySound;
+            switch (party)
+            {
+                case Parties.Optimism:
+                    _audioSource.pitch = 2.5f;
+                    break;
+                case Parties.Shyness:
+                    _audioSource.pitch = 1.5f;
+                    break;
+                case Parties.AI:
+                    _audioSource.pitch = 0.5f;
+                    break;
+                case Parties.Selfless:
+                    _audioSource.pitch = -0.5f;
+                    break;
+                case Parties.Anger:
+                    _audioSource.pitch = -1.5f;
+                    break;
+            }
         }
 
         private void PlaySound(Event gameEvent)
         {
-            
+            if (Results.Instance.ChosenSolutionForCurrentStep.supportingParties.Contains(party))
+                _audioSource.Play();
         }
     }
 }
